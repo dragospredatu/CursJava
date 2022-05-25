a0dc5cd0c84593f3761ce5c3bd6fa4cc54e09f2d
Dragos Predatu <dragos.predatu@yahoo.com>
2022-05-25 19:16:38 +0300

    initial commit

---
+++ src/curs1/FirstJavaClass.java
@@ -0,0 +1,24 @@
+package curs1;
+
+public class FirstJavaClass {
+
+	//variabila
+	
+	String nume = "Johnny";
+	
+	int age = 23;
+	
+	
+	
+	public static void main(String[] args) {
+		
+		FirstJavaClass obiect = new FirstJavaClass();
+		obiect.greetings();
+	}
+	
+	//metoda
+	public void greetings() {
+		System.out.println("Hello " + nume);
+	}
+
+}
