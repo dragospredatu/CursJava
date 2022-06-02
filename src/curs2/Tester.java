a503219fc12c234f709acbfca867bf8c2035d733
Dragos Predatu <dragos.predatu@yahoo.com>
2022-06-02 14:55:01 +0300

    added homework

--- src/curs2/Tester.java
+++ src/curs2/Tester.java
@@ -5,27 +5,40 @@ public class Tester {
 	//variabile
 	String nume;
 	int ratePerHour;
+	int age;
 	
 	//constructor default
 	//public Tester() {}
 	
-	public Tester(String nume, int ratePerHour) {
+	public Tester(String nume, int ratePerHour, int age) {
 		
 		setNume(nume);
 		setRatePerHour(ratePerHour);
+		setAge(age);
 	}
 	
 	//metode getter & setter
 	public String getNume() {
 		return nume;
 	}
+	
 	private void setNume(String nume) {
 		this.nume = nume;
 	}
+	
 	public int getRatePerHour() {
 		return ratePerHour;
 	}
+	
 	private void setRatePerHour(int ratePerHour) {
 		this.ratePerHour = ratePerHour;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+	private void setAge(int age) {
+		this.age = age;
 	}
 }
--- src/curs2/WageCalculator.java
+++ src/curs2/WageCalculator.java
@@ -6,13 +6,13 @@ public class WageCalculator {
 
 	public static void main(String[] args) {
 		
-		Tester tester1 = new Tester("Ion", 20);
+		Tester tester1 = new Tester("Ion", 20, 30);
 		//tester1.setNume("Ion");
 		
-		Tester tester2 = new Tester("Maria", 10);
+		Tester tester2 = new Tester("Maria", 10, 28);
 		//tester2.setNume("Maria");
 		
-		Tester tester3 = new Tester("Oana", 12);
+		Tester tester3 = new Tester("Oana", 12, 25);
 				
 		System.out.println(tester1.getNume().length());
 		System.out.println(tester2.getNume().length());
@@ -22,6 +22,10 @@ public class WageCalculator {
 		
 		System.out.println("Salariul lui "+ tester1.getNume() + " este " + 
 		calc.calculateSalary(tester1.getRatePerHour()));
+		
+		System.out.println(tester1.getNume() + " are: " + tester1.getAge() + " ani!");
+		System.out.println(tester2.getNume() + " are: " + tester2.getAge() + " ani!");
+		System.out.println(tester3.getNume() + " are: " + tester3.getAge() + " ani!");
 
 	}
 	
